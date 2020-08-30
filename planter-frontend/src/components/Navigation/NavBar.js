cb2ebd1df58d9a62d9d5fe5801cc9ca3afca627d
William Wang <williamwang@MacBook-Pro.lan>
2020-08-29 21:37:22 -0500

    minor updates

--- planter-frontend/src/components/Navigation/NavBar.js
+++ planter-frontend/src/components/Navigation/NavBar.js
@@ -3,16 +3,33 @@ import { Link, NavLink } from 'fusion-plugin-react-router';
 import paths from '../../constants/paths';
 
 const NavBar = () => {
+  // const activePath = () => {
+
+  // }
+
   return (
     <>
       <nav>
-        <NavLink activeClassName="inset" to={paths.home}>
+        <NavLink activeClassName="inset" exact to={paths.home}>
           home.
         </NavLink>
+        <NavLink activeClassName="inset" to={paths.garden}>
+          garden.
+        </NavLink>
+        <NavLink activeClassName="inset" to={paths.market}>
+          market.
+        </NavLink>
+        <NavLink activeClassName="inset" to={paths.shed}>
+          shed.
+        </NavLink>
+        <NavLink activeClassName="inset" to={paths.tasks}>
+          tasks.
+        </NavLink>
+        {/* <Link to={paths.home}>home.</Link>
         <Link to={paths.garden}>garden.</Link>
         <Link to={paths.market}>market.</Link>
         <Link to={paths.shed}>shed.</Link>
-        <Link to={paths.tasks}>tasks.</Link>
+        <Link to={paths.tasks}>tasks.</Link> */}
       </nav>
     </>
   );
